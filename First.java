58923c0b12ac903a3ac3b9ab38123bcc777974fc
TyTyKing06 <king_tyesia@students.columbusstate.edu>
2026-08-17 17:59:17 -0400

    Add First.java

---
+++ First.java
@@ -0,0 +1,12 @@
+//Name: Tyesia King
+//Assignment1: First Java
+
+public class First {
+    public static void main(String[] args) {
+    System.out.println("Hola mundo");
+    System.out.println("Job Title: Information Security Analyst");
+    System.out.println("Why: String job growth, high demand for cybersecurity skills, and the chance to protect organizations from real world threats.");
+    System.out.println("Median salary: $124,910 ($60.05 per hour)");
+    }
+   
+}
