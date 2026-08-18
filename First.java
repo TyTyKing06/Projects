9fa618592660e52bfe7b461ce27e13a3261f9a3e
TyTyKing06 <king_tyesia@students.columbusstate.edu>
2026-08-18 15:48:04 -0400

    AddTests.java: calculate average of 3 tests scores

--- First.java
+++ First.java
@@ -3,7 +3,7 @@
 
 public class First {
     public static void main(String[] args) {
-    System.out.println("Hola mundo");
+    System.out.println("Hola mundo"); " "
     System.out.println("Job Title: Information Security Analyst");
     System.out.println("Why: String job growth, high demand for cybersecurity skills, and the chance to protect organizations from real world threats.");
     System.out.println("Median salary: $124,910 ($60.05 per hour)");
