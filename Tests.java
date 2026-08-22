c99a108dc8c527b29fe72943543bcd95f269a870
TyTyKing06 <king_tyesia@students.columbusstate.edu>
2026-08-22 18:21:46 -0400

    Update Test.java

--- Tests.java
+++ Tests.java
@@ -1,12 +1,28 @@
+//Name: Tyesia King
+//Assignment2: Tests Java
+
+import java.util.Scanner;
+
 public class Tests {
     public static void main(String[] args) {
-        double testscore1= 75.2;
-        double testscore2= 83.4;
-        double testscore3= 90.0;
-        double average= (testscore1 + testscore2 + testscore3) / 3;
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print ("Enter Test Score 1: ");
+        double testscore1 = scnr.nextDouble();
+
+        System.out.print("Enter Test Score 2: ");
+        double testscore2 = scnr.nextDouble();
+
+        System.out.print("Enter Test Score 3: ");
+        double testscore3 = scnr.nextDouble();
+
+        double average = (testscore1 + testscore2 + testscore3) / 3;
         System.out.println("Test Score 1: " + testscore1);
         System.out.println("Test Score 2: " + testscore2);
         System.out.println("Test Score 3: " + testscore3);
         System.out.println("The average of 3 test scores is: " + average);
+    
+        scnr.close();
+    
     }
 }
